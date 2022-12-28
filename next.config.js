cc78e8b2305dd6f16202f595be37d6d5488c5157
RenanGava <renandellecrodegava7040@gmail.com>
2022-12-27 22:11:39 -0300

    Testatndo

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
 }
 
 module.exports = {
+  
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
